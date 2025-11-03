d63cc3004343b4fa0a5ce5ee2df217e07dca3ff2
JoaoVitorUTFPR <149010108+JoaoVitorUTFPR@users.noreply.github.com>
2025-11-03 17:41:07 -0300

    Login estilizado

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import { fazerLogin } from "../services/authService";
+import styles from "./login.module.css";
 
 export const Login = () => {
 
@@ -14,28 +15,26 @@ export const Login = () => {
             [e.target.name]: e.target.value
         });
     }
-    
-    const efetivarLogin = () => {
-        const token = fazerLogin(loginInfo)
-        if(token) {
+
+    const efetivarLogin = async () => {
+        const token = await fazerLogin(loginInfo)
+        if (token) {
             localStorage.setItem("token", token);
         }
     }
 
     return (
-        <>
+        <main className={styles.container}>
             <form onSubmit={efetivarLogin}>
-                <h2>Login</h2>
-                <div>
-                    <label>Email:</label>
-                    <input type="email" name="email" required onChange={setInfo}/>
+                <h1>Login</h1>
+                <div className={styles["input-box"]}>
+                    <input type="email" name="email" placeholder="Usuário" required onChange={setInfo}/>
                 </div>
-                <div>
-                    <label>Senha:</label>
-                    <input type="password" name="senha" required onChange={setInfo}/>
+                <div className={styles["input-box"]}>
+                    <input type="password" name="senha"  placeholder="Senha" required onChange={setInfo}/>
                 </div>
-                <button type="submit">Entrar</button>
+                <button type="submit">Login</button>
             </form>
-        </>
-    )
-}
+        </main>
+    );
+};
