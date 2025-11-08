e981692f360793db9e001c1dc44ce016bb92aa43
JoaoVitorUTFPR <149010108+JoaoVitorUTFPR@users.noreply.github.com>
2025-11-08 13:56:36 -0300

    simulado.jsx

--- frontend/src/pages/Simulados.jsx
+++ frontend/src/pages/Simulados.jsx
@@ -1,17 +1,16 @@
 import styles from "./Simulados.module.css";
-import Sidebar from "../components/Sidebar"; 
-import SimuladoList from "../components/simuladoList";
+import Sidebar from "../components/Sidebar";
+import SimuladoList from "../components/SimuladoList";
 
-function Simulados(){
+function Simulados() {
   return (
-    <>
-      <div className={styles.container}>
-        <Sidebar />
-        <div className={styles.content}>
-          {<SimuladoList/>}
-        </div>
+    <div className={styles.container}>
+      <Sidebar />
+      <div className={styles.content}>
+        <h2 className={styles.title}>Lista de Simulados</h2>
+        <SimuladoList />
       </div>
-    </>
+    </div>
   );
 }
 
